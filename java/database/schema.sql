74b621ddcab4bca2b7d202f58f60f95d3bc08108
Caleb Stockham <caleb23459@gmail.com>
2024-08-05 13:57:19 -0400

    UserShift model, dao, jdbc

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -45,7 +45,7 @@ CREATE TABLE vacation(
     CONSTRAINT FK_vacation_employee FOREIGN KEY (employee) REFERENCES users (user_id) ON DELETE CASCADE
 );
 
-CREATE TABLE user_shift (
+CREATE TABLE users_shift (
 	shift_id int NOT NULL,
 	coverer_id int NOT NULL,
 	CONSTRAINT PK_user_shift PRIMARY KEY (shift_id, coverer_id),
