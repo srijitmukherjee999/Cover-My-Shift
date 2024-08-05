4004a84e376819d1aa84fd2e5cf318d7d0ba3852
Colin Lutz <colinlutz513@gmail.com>
2024-08-05 11:31:13 -0400

    The time has come

---
+++ vue/src/views/PickUpShiftView.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Hello Colin</h1>
+  <nav>
+    <router-link v-bind:to="{name: 'employee'}">MY HOME</router-link>
+    &nbsp; | &nbsp;
+    <router-link v-bind:to="{name: 'timeoff'}">REQUEST TIME OFF</router-link>
+    &nbsp; | &nbsp;
+    <router-link v-bind:to="{name: 'pickupshift'}">PICK UP SHIFT</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
