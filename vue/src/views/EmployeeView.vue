26c6cf4cde5d0871dc8c43deca6d0e2a556030ae
Srijit Mukherjee <srijitmukherjee999@gmail.com>
2024-08-06 10:26:39 -0400

    push

--- vue/src/views/EmployeeView.vue
+++ vue/src/views/EmployeeView.vue
@@ -1,7 +1,6 @@
 <template>
      <body>
         
-   
     <div class="yes">
          <h1>Hello User</h1>
         <h1>Hours Worked: 40</h1>
@@ -23,9 +22,55 @@
 </template>
 
 <script>
+import ShiftService from '../services/ShiftService';
 import RequestTimeOffView from './RequestTimeOffView.vue';
 
+
 export default {
+
+    data(){
+        
+        return{
+            listOfShifts : [
+                {
+                    name : '',
+                    assigned : 0,
+                    userId: 0,
+                    startDateTime: '',
+                    duration: 0,
+                    status: 0,
+                    emergency:false,
+                    covererId: 0,
+                    description: ''
+                    
+                }
+            ],
+            name: []
+        }
+    }
+    ,
+
+    methods:{
+        getAllShifts(){
+            ShiftService.getShifts().then(response => {
+               this.listOfShifts = response.data;
+               this.getAllShifts();
+            })
+        },
+
+        getUser(id){
+            ShiftService.getUserByUserId(id).then(response => {
+              this.$store.state.user = response.data;
+            })
+        },
+        getNameByShift(){
+
+            this.listOfShifts.forEach(e => {
+              this.name = this.getUser(e.Id).name;
+            })
+
+        }
+    }
 
 }
 </script>
@@ -70,6 +115,7 @@ body{
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
+    min-height:100vh;
 }
 
 </style>
