0adfb65c8dff69b1fa34eb4feb83d7bda2350cd8
Caleb Stockham <caleb23459@gmail.com>
2024-08-06 11:42:51 -0400

    dummy data bugs fixed p3

--- java/database/data.sql
+++ java/database/data.sql
@@ -8,8 +8,8 @@ INSERT INTO users (username, password_hash, full_name, role, active) VALUES
 ('pwhite', 'hash5', 'Paul White', 'employee', TRUE);
 
 INSERT INTO shift (assigned, start_date_time, duration, status, emergency, coverer, description) VALUES
-((SELECT user_id FROM users WHERE username = 'jdoe'), '2024-08-05 08:00:00', 8, 1, FALSE, NULL, 'Regular shift'),
-((SELECT user_id FROM users WHERE username = 'asmith'), '2024-08-06 09:00:00', 6, 2, TRUE, 1, 'Emergency shift covered by John Doe'),
+((SELECT user_id FROM users WHERE username = 'jdoe'), '2024-08-05 08:00:00', 8, 2, FALSE, (SELECT user_id FROM users WHERE username = 'jdoe'), 'Regular shift'),
+((SELECT user_id FROM users WHERE username = 'asmith'), '2024-08-06 09:00:00', 6, 4, TRUE, (SELECT user_id FROM users WHERE username = 'jdoe'), 'Emergency shift covered by John Doe'),
 ((SELECT user_id FROM users WHERE username = 'mjones'), '2024-08-07 10:00:00', 5, 1, FALSE, NULL, 'Regular shift'),
 ((SELECT user_id FROM users WHERE username = 'klee'), '2024-08-08 07:00:00', 9, 3, FALSE, NULL, 'Overtime shift'),
 ((SELECT user_id FROM users WHERE username = 'pwhite'), '2024-08-09 08:00:00', 8, 1, TRUE, 2, 'Emergency shift covered by Alice Smith');
@@ -29,7 +29,8 @@ INSERT INTO vacation (employee, start_date, end_date, status) VALUES
 ((SELECT user_id FROM users WHERE username = 'pwhite'), '2024-08-05', '2024-08-15', 2);
 
 INSERT INTO users_shift (shift_id, coverer_id) VALUES
-((SELECT user_id FROM users WHERE username = 'asmith'), (SELECT shift_id FROM shift WHERE assigned = (SELECT user_id FROM users WHERE username = 'jdoe'))),
-((SELECT user_id FROM users WHERE username = 'pwhite'), (SELECT shift_id FROM shift WHERE assigned = (SELECT user_id FROM users WHERE username = 'asmith')));
+((SELECT shift_id FROM shift WHERE assigned = (SELECT user_id FROM users WHERE username = 'klee')),(SELECT user_id FROM users WHERE username = 'asmith')),
+((SELECT shift_id FROM shift WHERE assigned = (SELECT user_id FROM users WHERE username = 'klee')),(SELECT user_id FROM users WHERE username = 'pwhite')),
+((SELECT shift_id FROM shift WHERE assigned = (SELECT user_id FROM users WHERE username = 'klee')),(SELECT user_id FROM users WHERE username = 'jdoe'));
 
 COMMIT;
