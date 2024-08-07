433d9fe39c0fe7d529fd1fe593f849fe63e184bf
Colin Lutz <colinlutz513@gmail.com>
2024-08-07 11:06:44 -0400

    this is the way I live

--- vue/src/components/RequestOffForm.vue
+++ vue/src/components/RequestOffForm.vue
@@ -1,12 +1,15 @@
 <template>
+    <body>
+    <div id="form-body">
   <form class="request-off-form" v-on:submit.prevent="addNewRequest" v-if="showForm">
       <input type="date" class="start-date-input" placeholder="Date" v-model="newRequest.startDate">
       <input type="date" class="end-date-input" placeholder="Date" v-model="newRequest.endDate">
       <input type="text" class="description-input" placeholder="description" v-model="newRequest.description">
-      <button type="submit" >Submit Request</button>
+      <button type="submit">Submit Request</button>
     </form>
     <button v-else @click="toggleForm">Request Time Off</button>
-    
+</div>
+</body>
 </template>
 
 <script>
@@ -56,20 +59,78 @@ methods:{
 
 <style scoped>
 
-form{
-    text-align: center;
-    padding: 50px;
-    border: 5px;
-    border-color: orange;
+body, html {
+      height: 100%;
+      margin: 0;
+      display: flex;
+      justify-content: center; 
+      align-items: center;     
+    }
+
+#form-body {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+form {
+    display: flex;
+  flex-direction: column;
+  align-items: center;
+  align-content: center;
+  gap: 20px;
 }
 
 .request-off-form {
-    display: flex;
-    flex-direction: column;
-    max-width: 50%;
-    margin-left : 25%
-    
+    background-color: orange; 
+  color: white;
+  border-radius: 50px; 
+  padding: 35px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); 
+  width: 150%;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;  
+  box-sizing: border-box;
+  transition: transform 0.3s, box-shadow 0.3s;    
 }
 
+input[type="text"] {
+  width: 400px;         
+  height: 50px;         
+  padding: 10px;        
+  font-size: 18px;      
+  border: 2px solid #000; 
+  border-radius: 5px; 
+  text-align: center; 
+}
 
+input[type="date"] {
+  width: 400px;         
+  height: 50px;         
+  padding: 10px;        
+  font-size: 18px;      
+  border: 2px solid #000; 
+  border-radius: 5px;
+  text-align: center;  
+}
+
+button[type="submit"] {
+  width: 400px;         
+  height: 50px;         
+  padding: 10px;        
+  font-size: 18px;      
+  border: 2px solid #000; 
+  border-radius: 5px; 
+  font-size: large;
+}
+
+button:hover {
+    transform: scale(1.05); 
+  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.3);
+  background-color: lightgray;
+}
 </style>
