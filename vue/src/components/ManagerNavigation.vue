959909c841f7011e631d23567b4cfb32b55a690c
Colin Lutz <colinlutz513@gmail.com>
2024-08-09 10:12:57 -0400

    Great way to start the day

---
+++ vue/src/components/ManagerNavigation.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+      <nav class="navigation">
+        <ul>
+          <li><router-link v-bind:to="{ name: 'manager' }">MY HOME</router-link></li>
+          <li><router-link v-bind:to="{ name: 'pendingrequests' }">PENDING REQUESTS</router-link></li>
+          <li><router-link v-bind:to="{ name: 'pickupshift' }">FIRE EMPLOYEE</router-link></li>
+        </ul>
+      </nav>
+    </div>
+</template>
+
+<script>
+import ShiftService from '../services/ShiftService';
+
+export default {
+
+}
+</script>
+
+<style>
+
+.navigation {
+    padding: 10px;
+    margin: 20px;
+    border-radius: 5px;
+  }
+  
+  .navigation a {
+    text-decoration: none;
+    color: #000000;
+  }
+  
+  .navigation ul {
+    list-style: none;
+    padding: 0;
+    margin: center;
+    text-align: center;
+  }
+  
+  .navigation li {
+    display: inline;
+    margin-right: 15px;
+    font-size: larger;
+    background-color: white;
+    color: black;
+    border-radius: 50px;
+    padding: 20px;
+    box-shadow: 0 4px 8px;
+    width: 100%;
+    transition: transform 0.3s, box-shadow 0.3s;
+    font: bold;
+  }
+  
+  .navigation li:hover {
+    transform: scale(1.05);
+    box-shadow: 0 6px 12px rgba(0, 0, 0, 0.3);
+    background-color: lightgray;
+  }
+
+</style>
