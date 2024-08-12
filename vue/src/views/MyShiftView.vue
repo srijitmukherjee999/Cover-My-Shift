a3d53d9f9d4eb51b7bd0f9c46cd43544eec22dcf
Colin Lutz <colinlutz513@gmail.com>
2024-08-12 13:09:29 -0400

    tell me why

--- vue/src/views/MyShiftView.vue
+++ vue/src/views/MyShiftView.vue
@@ -408,7 +408,7 @@ h1 {
 
 .scrollable-container {
   position: fixed;
-  top: 390px; /* Adjust this based on your header height */
+  top: 310px; /* Adjust this based on your header height */
   left: 0;
   right: 0;
   bottom: 0;
